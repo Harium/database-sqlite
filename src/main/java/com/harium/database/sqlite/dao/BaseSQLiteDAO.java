0c3942cf8bf1b80f7f3d2baccc6bc34da0603cf4
Yuri Pourre <yuripourre@gmail.com>
2017-08-14 11:50:32 -0300

    Add OrmLite Database as dependency

--- src/main/java/com/harium/database/sqlite/dao/BaseSQLiteDAO.java
+++
@@ -1,8 +0,0 @@
-package com.harium.database.sqlite.dao;
-
-import com.harium.database.model.BaseDAO;
-import com.j256.ormlite.support.ConnectionSource;
-
-public interface BaseSQLiteDAO<T> extends BaseDAO<T, ConnectionSource> {
-
-}
